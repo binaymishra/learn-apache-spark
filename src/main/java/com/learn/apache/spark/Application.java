44b1c0d09dd8a012b41850aeb30caa46241d7b5c
Binay Mishra <binaymishramca@gmail.com>
2016-03-09 10:07:34 +0530

    Some more operation is added.

--- src/main/java/com/learn/apache/spark/Application.java
+++ src/main/java/com/learn/apache/spark/Application.java
@@ -1,5 +1,10 @@
 package com.learn.apache.spark;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -11,7 +16,7 @@ import org.apache.spark.api.java.JavaSparkContext;
  */
 public class Application {
 
-  private static final Logger LOGGER      = Logger.getLogger(Application.class);
+  private static final Logger LOGGER = Logger.getLogger(Application.class);
 
   private static String FILE_PATH = "./src/main/resources/big-data-file.txt";
 
@@ -22,9 +27,27 @@ public class Application {
 
     try {
       JavaRDD<String> textFile = context.textFile(FILE_PATH, 1);
-      LOGGER.info("Line count = "+textFile.count());
+      LOGGER.info("Line count = " + textFile.count());
 
-    } finally{
+      long thatWordCount = textFile.flatMap(t -> Arrays.asList(t.split(" ")))
+                                   .filter(word -> StringUtils.equalsIgnoreCase(word, "that"))
+                                   .count();
+
+      System.err.println(String.format("The word '%s' is %d times.", "that", thatWordCount));
+
+      Map<String, Long> wordCountMap = textFile.flatMap(t -> Arrays.asList(t.split("\\s+")))
+                                               .countByValue();
+
+      wordCountMap.forEach((key, value) -> System.err.println(String.format("%20s = %3s", key, value)));
+
+      List<String> wordsStartWithA =  textFile.flatMap(t -> Arrays.asList(t.split("\\s+")))
+                                              .map(word -> word.toUpperCase())
+                                              .filter(word -> word.startsWith("AN"))
+                                              .collect();
+
+      System.err.println(wordsStartWithA);
+
+    } finally {
       context.stop();
       context.close();
     }
